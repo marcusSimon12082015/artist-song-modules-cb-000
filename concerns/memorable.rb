dbf52a4937b38a047e2555e90789409cb12a7c60
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-04 16:21:09 +0000

    automatically backed up by learn

--- concerns/memorable.rb
+++ concerns/memorable.rb
@@ -1,12 +1,10 @@
 module Memorable
   module ClassMethods
-    attr_accessor :list
-    @@list = []
     def reset_all
         self.all.clear
     end
     def count
-        @@list.count
+        @@artists.count
     end
   end
 end
