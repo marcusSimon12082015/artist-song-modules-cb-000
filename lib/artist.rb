76842eac51c7b1ef77dcce853be2f8975d3e32f3
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-04 16:35:53 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,10 +6,6 @@ class Artist
   attr_reader :songs
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def initialize
     @@artists << self
     @songs = []
