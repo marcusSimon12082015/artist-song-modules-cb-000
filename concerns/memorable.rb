a7d72d816de8e0693fe429cf241d9574be7b4f79
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-04 16:31:07 +0000

    automatically backed up by learn

--- concerns/memorable.rb
+++ concerns/memorable.rb
@@ -1,10 +1,8 @@
 module Memorable
-  module ClassMethods
-    def reset_all
-        self.class.all.clear
-    end
-    def count
-        self.class.all.count
-    end
+  def reset_all
+      self.class.all.clear
   end
+  def count
+      self.class.all.count
+  end  
 end
